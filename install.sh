f6c301b44ea5789cd86cf2f9fcf8294544c4032f
Tijani Eneye Usman <brainyworld10@gmail.com>
2025-07-13 18:00:58 +0100

    wip

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
 
 # LEMP Stack Installation Script
-# Installs PHP 8.4, MySQL 8, Composer, and Nginx on Ubuntu/Debian
+# Installs PHP 8.4, MySQL 8, Composer, and Nginx on Ubuntu/Debian Server
 # Author: Tijani Usman
+# Version: 1.0.0
+# Date: 2025-07-13
+# License: MIT
+# Description: This script installs PHP 8.4, MySQL 8, Composer, and Nginx on Ubuntu/Debian Server
+# Prerequisites: Ubuntu/Debian Server
+# Dependencies: Nginx, PHP 8.4, MySQL 8, Composer
 
 set -e
 
@@ -58,3 +64,34 @@ apt-get install -y nginx
 systemctl start nginx
 systemctl enable nginx
 
+# Add PHP repository (Ondrej's PPA for latest PHP versions)
+log "Adding PHP repository..."
+add-apt-repository ppa:ondrej/php -y
+apt-get update -y
+
+# Install PHP 8.4 and common extensions
+log "Installing PHP 8.4 and extensions..."
+apt-get install -y \
+    php8.4 \
+    php8.4-fpm \
+    php8.4-mysql \
+    php8.4-curl \
+    php8.4-gd \
+    php8.4-mbstring \
+    php8.4-xml \
+    php8.4-zip \
+    php8.4-bcmath \
+    php8.4-soap \
+    php8.4-intl \
+    php8.4-readline \
+    php8.4-ldap \
+    php8.4-msgpack \
+    php8.4-igbinary \
+    php8.4-redis \
+    php8.4-memcached \
+    php8.4-xdebug
+
+systemctl start php8.4-fpm
+systemctl enable php8.4-fpm
+
+log "PHP 8.4 installed and configured"
